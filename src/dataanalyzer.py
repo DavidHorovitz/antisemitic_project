549c05b7b3444448dbd1a218297b4514c973bd5d
david horovitz <dovidho@gmail.com>
2025-08-04 16:51:32 +0300

    3 analizng func

--- src/dataanalyzer.py
+++ src/dataanalyzer.py
@@ -1,8 +1,7 @@
 import pandas as pd
 from pandas.core.interchange.dataframe_protocol import DataFrame
-from pandas.plotting import table
 
-from loader import Loader as lo
+
 class DataAnalyzer:
     def __init__(self,table: DataFrame):
         self.df=table
@@ -27,5 +26,43 @@ class DataAnalyzer:
         print("no_antisemit:", no_antisemit / count_of_no_antisemit)
         print((antisemit + no_antisemit) / (count_of_antisemit + count_of_no_antisemit))
 
+        def longest_text():
+            tweets_dict_antisemit = {}
+            tweets_dict_no_antisemit = {}
+            for i in range(0, len(self.df['Text']) - 1):
+                if self.df['Biased'][i] == 1:
+                    # len_of_antisemit= len(df['Text'][i].split(" "))
+                    len_of_antisemit = len(self.df['Text'][i])
+                    tweets_dict_antisemit[self.df['Text'][i]] = len_of_antisemit
+                else:
+                    len_of_no_antisemit = len(self.df['Text'][i])
+                    tweets_dict_no_antisemit[self.df['Text'][i]] = len_of_no_antisemit
+
+            arr_of_antisemit = []
+            arr_of_no_antisemit = []
+            for j in range(3):
+                maxy = max(tweets_dict_antisemit.values())
+                print(max(tweets_dict_antisemit.values()))
+                for key, valeu in tweets_dict_antisemit.items():
+                    if valeu == maxy:
+                        arr_of_antisemit.append(key)
+                        tweets_dict_antisemit[key] = 0
+            for k in range(3):
+                maxy = max(tweets_dict_no_antisemit.values())
+                print(max(tweets_dict_antisemit.values()))
+                for key, valeu in tweets_dict_no_antisemit.items():
+                    if valeu == maxy:
+                        arr_of_no_antisemit.append(key)
+                        tweets_dict_no_antisemit[key] = 0
+            for j in range(3):
+                maxy = max(tweets_dict_antisemit.values())
+                for key, valeu in tweets_dict_antisemit.items():
+                    if valeu == maxy:
+                        arr_of_no_antisemit.append(key)
+                        tweets_dict_antisemit[key] = 0
+            for text in arr_of_antisemit:
+                print("ANTISEMIT!", text)
+            for text in arr_of_no_antisemit:
+                print("NO ANTISEMIT!", text)
 
 
--- src/loader.py
+++ src/loader.py
@@ -1,9 +1,63 @@
+from pprint import pprint
+
 import pandas as pd
+
 
 class Loader:
     def loader(self):
         df = pd.read_csv("tweets_dataset.csv")
-        return df
+        # return df
+        tweets_dict_antisemit={}
+        tweets_dict_no_antisemit={}
+        for i in range(0, len(df['Text']) - 1):
+            if df['Biased'][i] == 1:
+                # len_of_antisemit= len(df['Text'][i].split(" "))
+                len_of_antisemit= len(df['Text'][i])
+                tweets_dict_antisemit[df['Text'][i]] = len_of_antisemit
+
+
+            else:
+                len_of_no_antisemit = len(df['Text'][i])
+                tweets_dict_no_antisemit[df['Text'][i]] = len_of_no_antisemit
+
+
+
+        arr_of_antisemit = []
+        arr_of_no_antisemit = []
+        for j in range(3):
+            maxy = max(tweets_dict_antisemit.values())
+            print(max(tweets_dict_antisemit.values()))
+            for key, valeu in tweets_dict_antisemit.items():
+                if valeu == maxy:
+                    arr_of_antisemit.append(key)
+                    tweets_dict_antisemit[key] = 0
+        for k in range(3):
+            maxy = max(tweets_dict_no_antisemit.values())
+            print(max(tweets_dict_antisemit.values()))
+            for key, valeu in tweets_dict_no_antisemit.items():
+                if valeu == maxy:
+                    arr_of_no_antisemit.append(key)
+                    tweets_dict_no_antisemit[key] = 0
+        for j in range(3):
+            maxy = max(tweets_dict_antisemit.values())
+            for key, valeu in tweets_dict_antisemit.items():
+                if valeu == maxy:
+                    arr_of_no_antisemit.append(key)
+                    tweets_dict_antisemit[key] = 0
+        for text in arr_of_antisemit:
+            print("ANTISEMIT!", text)
+        for text in arr_of_no_antisemit:
+            print("NO ANTISEMIT!", text)
+
+
+
+
+
+
+
+
+
+
 
 
 
